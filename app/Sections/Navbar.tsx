27c3e69af16c2a7d5fe5563cb6d281d04bc669e9
Mohammedsalman3052004 <mdsalman3052004@gmail.com>
2025-07-31 01:22:05 +0530

    Navbar Completed

--- app/Sections/Navbar.tsx
+++ app/Sections/Navbar.tsx
@@ -1,12 +1,46 @@
 import React from 'react'
 import '../Styles/navbar.css'
+import Image from 'next/image'
+import navbarLogo from "@/public/Images/nav-logo-ellipse-1.png"
+import Instagram from "@/public/Icons/instagram.svg"
+import Facebook from "@/public/Icons/facebook.svg"
+import LinkedIN from "@/public/Icons/linkedIn.svg"
+import Youtube from "@/public/Icons/youtube.svg"
 
 const Navbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <nav className='navbar'>
+            <div className='navbar-container'>
+                <div className='nav-logo-wrapper'>
+                    <div className='nav-logo-title'>
+                       musharraff
+                    </div>
+                    <div className='nav-logo-bottom'>
+                        <div className='nav-logo-subtitle'>
+                            maliq
+                        </div>
+                        <div className='nav-logo-image'>
+                            <Image src={navbarLogo} alt='Navbar Logo' height={28} width={28} />
+                        </div>
+                    </div>
+                </div>
+                <div className='nav-links-wrapper'>
+                    <a href="#home" className='nav-link active'>Home</a>
+                    <a href="#about" className='nav-link'>About</a>
+                    <a href="#head-photos" className='nav-link'>Head photos</a>
+                    <a href="#monologue" className='nav-link'>Monologue</a>
+                    <a href="#look-book" className='nav-link'>Look book</a>
+                    <a href="#contact" className='nav-link'>Contact</a>
+                </div>
+                <div className='nav-icons-wrapper'>
+                    <Image src={Instagram} alt='Navbar Logo' height={18} width={18} />
+                    <Image src={Facebook} alt='Navbar Logo' height={18} width={18} />
+                    <Image src={LinkedIN} alt='Navbar Logo' height={18} width={18} />
+                    <Image src={Youtube} alt='Navbar Logo' height={18} width={18} />
+                </div>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from 'next/font/google';
+import { Bebas_Neue } from 'next/font/google';
+import { Chelsea_Market } from 'next/font/google';
 import "./globals.css";
 
 const geistSans = Geist({
@@ -12,10 +15,31 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter', // Custom CSS variable
+  display: 'swap',
+});
+
+const bebasNeue = Bebas_Neue({
+  weight: '400', // Only one weight available
+  subsets: ['latin'],
+  variable: '--font-bebas',
+  display: 'swap',
+});
+
+const chelseaMarket = Chelsea_Market({
+  weight: '400',
+  subsets: ['latin'],
+  variable: '--font-chelsea',
+  display: 'swap',
+});
+
 export const metadata: Metadata = {
   title: "Mussaraf Portfolio",
   description: "Mussaraf Portfolio Website",
 };
+
 
 export default function RootLayout({
   children,
@@ -25,7 +49,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} ${bebasNeue.variable} ${chelseaMarket.variable} antialiased`}
+        
       >
         {children}
       </body>

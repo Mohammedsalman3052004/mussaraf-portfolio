e9bc50cac100de033bb69d9540b6842a6c117b17
Mohammedsalman3052004 <mdsalman3052004@gmail.com>
2025-07-31 00:32:23 +0530

    Initial Commit

---
+++ app/Sections/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Styles/navbar.css'
+
+const Navbar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Navbar
---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Sections/Navbar'
+import Footer from './Sections/Footer'
+
+const page = () => {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  )
+}
+
+export default page
